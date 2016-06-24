a756ad150e3fc75049a3447c447bc59645853e6d
durgakumari <durga.kumari@thermofisher.com>
2016-06-24 07:46:59 +0530

    Added more tags.

--- src/test/java/org/bsc/test/doclet/JerseyService.java
+++ src/test/java/org/bsc/test/doclet/JerseyService.java
@@ -4,32 +4,79 @@
  */
 package org.bsc.test.doclet;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.xml.ws.http.HTTPException;
 
 /**
  *
  * @author softphone
  */
-@Path( "test")
-public class JerseyService {
-    
-    
+@Path("/test/jersey")
+public interface JerseyService {
+
+
     /**
-     * Test service
-     * 
-     * 
-     * @param param1 Test param
-     * @param enabled True|False
-     * @since 1.0
-     * @deprecated  use service2 instead
-     * @return return generic string
+     * Service1 Description
+     * Testing.......
+     *
+     *
+     * @return 200 OK
+     *
+     * @see
+     * SampleInput
+     * {
+     *     "input1":"val1",
+     *     "input2":"val2",
+     *     "input3":"val3",
+     *     "input4":"val4",
+     *     "input5":"val5",
+     *     "input6":"val6"
+     * }
+     *
+     * SampleOutput
+     * {
+     *     "output1":"oval1",
+     *     "output2":"oval2",
+     *     "output3":"oval3",
+     *     "output4":"oval4",
+     *     "output5":"oval5",
+     *     "output6":"oval6"
+     * }
+     *
      */
-    @GET
+
+    @POST
     @Deprecated
-    public String service1( String param1, boolean enabled )
-    {
-        return null;
-    }
-    
+    @Path("/testservice1")
+    @Produces(MediaType.MULTIPART_FORM_DATA)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Long service1(@QueryParam("param1") String param1, POJOSample pojoSample, int paramTesting) throws HTTPException, Exception;
+
+
+    /**
+     * Service2 Description
+     * Testing.......
+     *
+     * @return 200 OK
+     * @throws 500 403
+     * @see "SampleInput:
+     * {
+     * "Test":"Testing"
+     * }
+     * <p/>
+     * SampleOutput:
+     * {
+     * "Output":"Output"
+     * }"
+     * @deprecated
+     */
+
+    @POST
+    @Deprecated
+    @Path("/testservice2/{param1}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public POJOSample serviceTest2(@QueryParam("qparam") String qparam, POJOSample pojoSample, @PathParam("param1") Long param1);
+
 }
